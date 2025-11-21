dcf26afa489dba77be9c0248eeded3d27afbe116
Monroe Gordon <monroegordon1987@gmail.com>
2025-11-21 14:52:45 -0700

    Add files via upload

--- tools/reduction.py
+++ tools/reduction.py
@@ -1,7 +1,45 @@
+from algorithms.lda import LDA
 from linalg.decomposition import svd
+from linalg.eigen import eigen
 
 import cupy as cp
 import numpy as np
+
+def lda_projection(x: np.ndarray | cp.ndarray,
+                   y: list,
+                   n_components: int,
+                   device: str = "cpu") -> tuple:
+    '''
+    Perform linear discriminant analysis projection.
+    :param x: Input feature matrix (number samples, number features).
+    :param y: Class labels (number samples).
+    :param n_components: Desired number of dimensions.
+    :param device: CPU or GPU device.
+    :return: Transformed feature matrix and the projection matrix.
+    '''
+    if x.ndim != 2:
+        raise ValueError("@ lda_projection: parameter 'x' must be a matrix (2-dimensional ndarray).")
+
+    means = LDA.means(x, y, device)
+    sw = LDA.within_class_scatter(x, y, means, device)
+    sb = LDA.between_class_scatter(x, y, means, device)
+
+    if device == "cpu":
+        e_vals, e_vecs = eigen(np.linalg.inv(sw).dot(sb))
+
+        sorted_indices = np.argsort(e_vals)[::-1]
+        e_vecs = e_vecs[:, sorted_indices]
+
+        w = e_vecs[:, :n_components]
+    else:
+        e_vals, e_vecs = eigen(cp.linalg.inv(sw).dot(sb))
+
+        sorted_indices = cp.argsort(e_vals)[::-1]
+        e_vecs = e_vecs[:, sorted_indices]
+
+        w = e_vecs[:, :n_components]
+
+    return x.dot(w), w
 
 def svd_dim_reduction(matrix: np.ndarray | cp.ndarray,
                       k: int,
