a5b9e7eb403a355a854ec783a6043197b0a5ec7b
Monroe Gordon <monroegordon1987@gmail.com>
2025-11-22 22:44:34 -0700

    Add files via upload

--- algorithms/knn.py
+++ algorithms/knn.py
@@ -14,7 +14,7 @@ class KNN:
 
     @staticmethod
     def predict(x_train: np.ndarray | cp.ndarray,
-                y_train: list,
+                y_train: np.ndarray | cp.ndarray,
                 x_test: np.ndarray | cp.ndarray,
                 k: int,
                 device: str="cpu") -> np.ndarray | cp.ndarray:
@@ -31,21 +31,25 @@ class KNN:
 
         if device == "cpu":
             nx_train = x_train
+            ny_train = y_train
             nx_test = x_test
 
             if isinstance(nx_train, cp.ndarray):
                 nx_train = cp.asnumpy(nx_train)
 
+            if isinstance(ny_train, cp.ndarray):
+                ny_train = cp.asnumpy(ny_train)
+
             if isinstance(nx_test, cp.ndarray):
                 nx_test = cp.asnumpy(nx_test)
 
             for i in range(nx_test.shape[0]):
-                test_points = np.full(nx_train.shape[0], nx_test[i])
+                test_points = nx_test[i]
                 distances = euclidean_dist(test_points, nx_train)
 
                 k_indices = np.argsort(distances)[:k]
 
-                k_nearest_labels = [y_train[i] for i in k_indices]
+                k_nearest_labels = [ny_train[i] for i in k_indices]
 
                 most_common = Counter(k_nearest_labels).most_common(1)
                 predictions.append(most_common[0][0])
@@ -53,21 +57,25 @@ class KNN:
             return np.array(predictions)
         else:
             cx_train = x_train
+            cy_train = y_train
             cx_test = x_test
 
             if isinstance(cx_train, np.ndarray):
                 cx_train = cp.asarray(cx_train)
 
-            if isinstance(cx_test, cp.ndarray):
+            if isinstance(cy_train, np.ndarray):
+                cy_train = cp.asarray(cy_train.astype(np.float32))
+
+            if isinstance(cx_test, np.ndarray):
                 cx_test = cp.asarray(cx_test)
 
             for i in range(x_test.shape[0]):
-                test_points = cp.full(cx_train.shape[0], cx_test[i])
+                test_points = cx_test[i]
                 distances = euclidean_dist(test_points, cx_train)
 
                 k_indices = cp.argsort(distances)[:k]
 
-                k_nearest_labels = [y_train[i] for i in k_indices]
+                k_nearest_labels = [float(cy_train[i]) for i in k_indices]
 
                 most_common = Counter(k_nearest_labels).most_common(1)
                 predictions.append(most_common[0][0])
@@ -142,22 +150,24 @@ class KNN:
 
     @staticmethod
     def find_optimal_k(x: np.ndarray | cp.ndarray,
-                       y: list,
-                       max_k: int) -> int:
+                       y: np.ndarray | cp.ndarray,
+                       max_k: int,
+                       device: str="cpu") -> int:
         '''
         Find the optimal K value for K-nearest neighbors using cross-validation.
         :param x: Data features.
         :param y: Data labels.
         :param max_k: Maximum K value to test.
+        :param device: CPU or GPU device.
         :return: Best K value based on accuracy.
         '''
         best_k = 1
         best_accuracy = 0
 
         for k in range(1, max_k + 1):
-            x_train, x_test, y_train, y_test = train_test_split(x, np.array(y), test_size=0.3, random_state=42)
-            y_pred = KNN.predict(x_train, y_train, x_test, k)
-            acc = accuracy(y_test, y_pred)
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+            y_pred = KNN.predict(x_train, y_train, x_test, k, device)
+            acc = accuracy(y_test, y_pred, device)
 
             if acc > best_accuracy:
                 best_accuracy = acc
